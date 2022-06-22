e3c1fe584d156d69487346f647d98e29c3dab061
Roger Herany <caminharoger@gmail.com>
2022-06-22 02:08:53 +0100

    Update te001.py

--- bobeira/te001.py
+++ bobeira/te001.py
@@ -2,7 +2,7 @@ import os
 palpites=list()
 palpites2=tuple()
 count=0
-
+zero=1
 
 while True:
     count+=1
@@ -12,9 +12,16 @@ while True:
     if num == '999':
         break
     else:
-        palpites.append(num)
+        if len(palpites)>0:
+            if num==palpites[len(palpites)-1]:
+                zero+=1
+                palpites[len(palpites)-1]=f'0*{zero}'                
+            else:
+                palpites.append(num)
 
-        #proxima vez usar um ciclo for para verificar se os numeros seguintes são iguais, e usar github branches
+        else:
+            palpites.append(num)
+        
 os.system("cls")
 
-print(f'Palpites: {palpites}')
+print(f'Palpites: {palpites}, {zero}')
